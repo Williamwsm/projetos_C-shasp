e93a24a04bee6d521728edfe9041e9bed423f4a5
William Santos <113859999+Williamwsm@users.noreply.github.com>
2024-11-10 11:56:16 -0300

    add metodos

---
+++ AgendaTelefonica/Agenda.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Contatos;
+
+public class Agenda
+{
+	private Contatos listaContatos = new ArrayList();
+	public Agenda()
+	{
+	}
+	public void  cadastrarContato(Contatos contatos)
+	{
+		listaContatos.Add(contatos);
+
+	}
+	 public void removerContato(Contatos contatos)
+	{
+		listaContatos.Remove(contatos);
+
+	}
+	public bool buscarContaos(Contatos contatos)
+	{
+		foreach(var contato in listaContatos)
+		{
+			if (contato.Equals(contatos))
+			{
+				return true;
+				break
+			}
+			
+		}
+		return false;
+	}
+
+}
---
+++ AgendaTelefonica/Contatos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Contatos{
+    private string nome {  get; set; }
+    private int telefone {  get; set; }
+    private string email {  get; set; }
+
+    public Contatos(string
+         nome, int telefone, string email){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+	}
+    public override string ToString() { return $"Nome: {nome}, Telefone:{telefone}, Email: {email}"}
+}
